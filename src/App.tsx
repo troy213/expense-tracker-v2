156e39dc391233ee5b6e80dd38b7316040a2921c
troy213 <tritera.erlangga@gmail.com>
2024-05-05 01:49:47 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.scss'
+
+const App = () => {
+  return <>Hello World</>
+}
+
+export default App
---
+++ src/assets/index.ts
@@ -0,0 +1,39 @@
+import ArrowDownSvg from './icons/arrow-down.svg?react'
+import ArrowLeftSvg from './icons/arrow-left.svg?react'
+import ArrowUpSvg from './icons/arrow-up.svg?react'
+import CalendarSvg from './icons/calendar.svg?react'
+import ChevronDownSvg from './icons/chevron-down.svg?react'
+import CoinsSvg from './icons/coins.svg?react'
+import ExportSvg from './icons/export.svg?react'
+import FlagIDSVg from './icons/flag-id.svg?react'
+import FlagUKSvg from './icons/flag-uk.svg?react'
+import GlobeSvg from './icons/globe.svg?react'
+import ImportSvg from './icons/import.svg?react'
+import MoreVerticalSvg from './icons/more-vertical.svg?react'
+import PaletteSvg from './icons/palette.svg?react'
+import PieChartSvg from './icons/pie-chart.svg?react'
+import PlusSvg from './icons/plus.svg?react'
+import SearchSvg from './icons/search.svg?react'
+import SettingsSvg from './icons/settings.svg?react'
+import SlidersSvg from './icons/sliders.svg?react'
+
+export {
+  ArrowDownSvg,
+  ArrowLeftSvg,
+  ArrowUpSvg,
+  CalendarSvg,
+  ChevronDownSvg,
+  CoinsSvg,
+  ExportSvg,
+  FlagIDSVg,
+  FlagUKSvg,
+  GlobeSvg,
+  ImportSvg,
+  MoreVerticalSvg,
+  PaletteSvg,
+  PieChartSvg,
+  PlusSvg,
+  SearchSvg,
+  SettingsSvg,
+  SlidersSvg,
+}
