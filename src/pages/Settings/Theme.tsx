dc74f0fdb8b95c918924fd385be0f8902f7d5883
troy213 <tritera.erlangga@gmail.com>
2024-08-09 17:34:14 +0700

    Feat: Husky testing

--- src/pages/Settings/Theme.tsx
+++ src/pages/Settings/Theme.tsx
@@ -1,54 +1,53 @@
-import { Navbar } from "@/components"
-import useAppDispatch from "@/hooks/useAppDispatch"
-import useAppSelector from "@/hooks/useAppSelector"
-import { themeAction } from "@/store/theme/theme-slice"
-
+import { Navbar } from '@/components'
+import useAppDispatch from '@/hooks/useAppDispatch'
+import useAppSelector from '@/hooks/useAppSelector'
+import { themeAction } from '@/store/theme/theme-slice'
 
 const Theme: React.FC = () => {
-  const theme = useAppSelector((state) => state.themeReducer.theme);
+  const theme = useAppSelector((state) => state.themeReducer.theme)
   const dispatch = useAppDispatch()
   const OnThemeChange = () => {
     dispatch(themeAction.toggleTheme())
-  };
+  }
 
   return (
-      <div className="flex-column gap-8 p-4">
-        <Navbar title="Theme" enableBackButton={true} />
+    <div className="flex-column gap-8 p-4">
+      <Navbar title="Theme" enableBackButton={true} />
 
-        <ul className="flex-column gap-8 py-4">
-          <li>
-            <button type="button" className="btn btn-clear">
-              <div className="flex-align-center gap-2">
-                <input
-                  type="radio"
-                  id="light"
-                  name="theme"
-                  value="light"
-                  checked={theme === "light"}
-                  onChange={OnThemeChange}
-                />
-                <label htmlFor="light">Light Theme</label>
-              </div>
-            </button>
-          </li>
-          <li>
-            <button type="button" className="btn btn-clear">
-              <div className="flex-align-center gap-2">
-                <input
-                  type="radio"
-                  id="dark"
-                  name="theme"
-                  value="dark"
-                  checked={theme === "dark"}
-                  onChange={OnThemeChange}
-                />
-                <label htmlFor="dark">Dark Theme</label>
-              </div>
-            </button>
-          </li>
-        </ul>
-      </div>
-  );
-};
+      <ul className="flex-column gap-8 py-4">
+        <li>
+          <button type="button" className="btn btn-clear">
+            <div className="flex-align-center gap-2">
+              <input
+                type="radio"
+                id="light"
+                name="theme"
+                value="light"
+                checked={theme === 'light'}
+                onChange={OnThemeChange}
+              />
+              <label htmlFor="light">Light Theme</label>
+            </div>
+          </button>
+        </li>
+        <li>
+          <button type="button" className="btn btn-clear">
+            <div className="flex-align-center gap-2">
+              <input
+                type="radio"
+                id="dark"
+                name="theme"
+                value="dark"
+                checked={theme === 'dark'}
+                onChange={OnThemeChange}
+              />
+              <label htmlFor="dark">Dark Theme</label>
+            </div>
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default Theme
--- src/store/theme/theme-slice.ts
+++ src/store/theme/theme-slice.ts
@@ -1,32 +1,29 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 // import { Category, SetStatePayload } from "@/types"
 
 type InitialState = {
   theme: 'light' | 'dark'
-};
+}
 
 const initialState: InitialState = {
-  theme: "light"
-};
+  theme: 'light',
+}
 
 const themeSlice = createSlice({
-  name: "theme",
+  name: 'theme',
   initialState,
   reducers: {
-    toggleTheme(state){
-        state.theme = state.theme === 'light' ? 'dark' : 'light'
+    toggleTheme(state) {
+      state.theme = state.theme === 'light' ? 'dark' : 'light'
     },
-    setState(
-      state: InitialState,
-      action: PayloadAction<'light' | 'dark'>
-    ) {
+    setState(state: InitialState, action: PayloadAction<'light' | 'dark'>) {
       state.theme = action.payload
     },
     resetState() {
       return initialState
     },
   },
-});
+})
 
 export const themeAction = themeSlice.actions
 
