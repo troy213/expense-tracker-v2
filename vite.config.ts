dca30e1a8f509e6b62a0c196f8b48ecdc27d42d3
adrleo24 <adrianus@axion.ltd>
2025-01-24 22:21:31 +0700

    feat: setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,49 @@ import eslint from 'vite-plugin-eslint'
 import checker from 'vite-plugin-checker'
 import path from 'path'
 import { version } from './package.json'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), eslint(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Expense Tracker',
+        short_name: 'Expense Tracker',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+    svgr(),
+    eslint(),
+    checker({ typescript: true }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
